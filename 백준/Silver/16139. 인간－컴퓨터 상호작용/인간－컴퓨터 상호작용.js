39f3e9be692f56a49db55f4650171713bf4658f0
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-02-24 20:50:50 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 932 ms, Memory: 152072 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -6,36 +6,20 @@ const questions = rest.map((row) => row.split(' '));
 
 const prefixSumByAlphabet = Array.from({length: 26}, () => Array.from({length: S.length + 1}).fill(0));
 
-// const findAlphabetCount = (alphabet, endPoint) => {
-//   const sliceS = splitS.slice(0, endPoint);
-//   console.log(sliceS)
-//   const map = new Map();
-//   map.set()
-  
-//   const count = sliceS.filter((alpha) => alpha === alphabet).length;
-//   return count
-// }
-
-
-for (let i = 97; i <= 122; i++) {
-    const alphabet = String.fromCharCode(i);
+for (let i = 0; i < 26; i++) {
+    const alphabet = String.fromCharCode(97 + i);
     for (let j = 1; j <= S.length; j++) {
-        const count = S[j - 1] === alphabet ? 1 : 0;
-        prefixSumByAlphabet[i - 97][j] = prefixSumByAlphabet[i - 97][j - 1] + count;
-        
+        prefixSumByAlphabet[i][j] = prefixSumByAlphabet[i][j - 1] + (S[j - 1] === alphabet ? 1 : 0);
     }
 }
 
+let result = [];
+questions.forEach(([alphabet, left, right]) => {
+    const index = alphabet.charCodeAt(0) - 97;
+    left = Number(left);
+    right = Number(right);
 
-questions.forEach((question) => {
-    const [alphabet, ...range] = question;
-    const [left, right] = range.map(Number);
-    const alphabetASCIIIndex = alphabet.charCodeAt() - 97;
-
-    if (right - left === 0) {
-      console.log(S[right] === alphabet ? 1 : 0);
-    } else {
-      console.log(prefixSumByAlphabet[alphabetASCIIIndex][right + 1] - prefixSumByAlphabet[alphabetASCIIIndex][left]);
-    }
-    
+    result.push(prefixSumByAlphabet[index][right + 1] - prefixSumByAlphabet[index][left]);
 });
+
+console.log(result.join('\n'));
