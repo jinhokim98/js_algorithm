ea52b69a7d9d558a5499867f3346b17921d8158c
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-03-01 22:17:20 +0900

    [Silver II] Title: 최소 힙, Time: 236 ms, Memory: 33352 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.js
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -1,46 +1,43 @@
-class Heap {
+class Heap { 
     constructor() {
-        this.heap = [];
+        this.heap = []
     }
     
     getLeftChildIndex = (parentIndex) => parentIndex * 2 + 1;
     getRightChildIndex = (parentIndex) => parentIndex * 2 + 2;
     getParentIndex = (childIndex) => Math.floor((childIndex - 1) / 2);
-
-    peek = () => this.heap[0];
-
-    insert = (key, value) => {
+    
+    push = (key, value) => {
         const node = {key, value};
         this.heap.push(node);
-        this.heapifyUp(); // 배열의 끝에 넣고 다시 min heap으로
+        this.heapifyUp();
     }
     
     heapifyUp = () => {
-        let index = this.heap.length - 1; // 끝의 인덱스
+        let index = this.heap.length - 1;
         const lastInsertedNode = this.heap[index];
         
         while (index > 0) {
             const parentIndex = this.getParentIndex(index);
             
-            // 부모보다 자식이 크다면 자식과 부모의 순서를 바꾼다.
             if (this.heap[parentIndex].key > lastInsertedNode.key) {
                 this.heap[index] = this.heap[parentIndex];
-                index = parentIndex; // 바꾼 후 현재 인덱스를 부모로 옮기기
+                index = parentIndex;
             } else break;
         }
         
-        // 마지막 찾은 곳이 들어올 곳
         this.heap[index] = lastInsertedNode;
     }
     
-    remove = () => {
-        const count = this.heap.length;
-        const rootNode = this.heap[0];
+    pop = () => {
+        let index = 0;
+        const size = this.heap.length;
+        const rootNode = this.heap[index];
         
-        if (count <= 0) return undefined;
-        if (count === 1) this.heap = [];
+        if (size === 0) return undefined;
+        if (size === 1) return this.heap.pop();
         else {
-            this.heap[0] = this.heap.pop(); // 끝 노드를 가져와서 루트로 만든다.
+            this.heap[0] = this.heap.pop();
             this.heapifyDown();
         }
         
@@ -49,19 +46,16 @@ class Heap {
     
     heapifyDown = () => {
         let index = 0;
-        const count = this.heap.length;
+        const size = this.heap.length;
         const rootNode = this.heap[index];
         
-        // 계속해서 left child가 있을 때까지 검사한다.
-        while (this.getLeftChildIndex(index) < count) {
+        while (this.getLeftChildIndex(index) < size) {
             const leftChildIndex = this.getLeftChildIndex(index);
             const rightChildIndex = this.getRightChildIndex(index);
             
-            // 왼쪽 오른쪽 중에 더 작은 노드를 구한다.
-            const smallerChildIndex = rightChildIndex < count && this.heap[rightChildIndex].key < this.heap[leftChildIndex].key ? rightChildIndex : leftChildIndex;
+            const smallerChildIndex = rightChildIndex < size && this.heap[rightChildIndex].key < this.heap[leftChildIndex].key ? rightChildIndex : leftChildIndex;
             
-            // 자식 노드의 키 값이 루트노드보다 작다면 위로 올린다.
-            if (this.heap[smallerChildIndex].key <= rootNode.key) {
+            if (this.heap[smallerChildIndex].key < rootNode.key) {
                 this.heap[index] = this.heap[smallerChildIndex];
                 index = smallerChildIndex;
             } else break;
@@ -69,30 +63,22 @@ class Heap {
         
         this.heap[index] = rootNode;
     }
-}
-
-class PriorityQueue extends Heap {
-    constructor() {
-        super();
-    }
     
-    push = (priority, value) => this.insert(priority, value);
-    pop = () => this.remove();
     isEmpty = () => this.heap.length <= 0;
 }
 
 const fs = require('fs');
-const [n, ...rest] = fs.readFileSync(0, 'utf8').trim().split('\n').map(Number);
-
-const pq = new PriorityQueue();
+const [n, ...rest] = fs.readFileSync(0, 'utf8').trim().split('\n');
+const pq = new Heap();
 const answer = [];
 
-rest.forEach((number) => {
+rest.forEach((line) => {
+    const number = parseInt(line, 10);
     if (number === 0) {
         answer.push(pq.pop()?.value ?? 0);
-    } else {
+    } else if (number !== 0) {
         pq.push(number, number);
     }
 });
 
-console.log(answer.join('\n'))
+console.log(answer.join('\n'));
