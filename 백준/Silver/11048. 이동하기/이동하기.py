ef5417a9ebaa5d16432dc9dee9876b4416131144
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-03-26 22:51:04 +0900

    [Silver II] Title: 이동하기, Time: 656 ms, Memory: 79472 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+d = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        d[i][j] = max(d[i - 1][j], d[i][j - 1]) + matrix[i - 1][j - 1]
+
+print(d[n][m])
