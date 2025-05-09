67ead28a389567479c4c9e039bb32f04f978c607
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-05-09 20:56:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 644 ms, Memory: 138892 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const [input, rest] = fs.readFileSync(0, 'utf8').trim().split('\n')
+const n = parseInt(input, 10)
+const sequence = rest.split(' ').map(Number)
+
+const lis = []
+
+sequence.forEach((number) => {
+    let left = 0
+    let right = lis.length
+    
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+        if (lis[mid] < number) left = mid + 1
+        else right = mid
+    }
+    
+    if (left === lis.length) lis.push(number)
+    else lis[left] = number
+})
+
+console.log(lis.length)
