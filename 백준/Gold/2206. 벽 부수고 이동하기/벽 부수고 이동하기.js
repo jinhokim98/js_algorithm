040a199472e66dfffc8d61dd2d63cba76ff88152
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-05-09 11:49:28 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2560 ms, Memory: 313460 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -33,9 +33,10 @@ const [input, ...rest] = fs.readFileSync(0, 'utf8').trim().split('\n');
 
 const [n, m] = input.split(' ').map(Number);
 const matrix = rest.map((row) => row.split('').map(Number));
-const visited = Array.from({length: n}, () => Array.from({length: m}, () => [0, 0]));
+const visited = Array.from({ length: n }, () =>
+  Array.from({ length: m }, () => [Infinity, Infinity])
+);
 const directions = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-
 
 const bfs = (startX, startY) => {   
     const queue = new Queue();
@@ -45,6 +46,7 @@ const bfs = (startX, startY) => {
     while(queue.size() > 0) {
         const [x, y, isPunch] = queue.dequeue();
         
+        // 도착 지점에 도달하면
         if (x === n-1 && y === m-1) {
           return visited[x][y][isPunch];
         }
@@ -52,24 +54,26 @@ const bfs = (startX, startY) => {
         for (const [dx, dy] of directions) {
             const nx = x + dx;
             const ny = y + dy;
-            
-            // 방문하지 않은 유효한 점
-            if (nx >= 0 && nx < n && ny >=0 && ny < m && visited[nx][ny][isPunch] === 0) {
-                const nextDistance = visited[nx][ny][isPunch] + 1;
-                // 0이라면
-                if (matrix[nx][ny] === 0 && (visited[nx][ny][isPunch] === 0 || visited[nx][ny][isPunch] > nextDistance)) {
-                    visited[nx][ny][isPunch] = visited[x][y][isPunch] + 1;
+
+            // 유효한 좌표이고, 벽을 부수지 않은 경로와 벽을 부순 경로를 모두 탐색
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                const nextDistance = visited[x][y][isPunch] + 1;
+
+                // 벽이 아닌 곳
+                if (matrix[nx][ny] === 0 && visited[nx][ny][isPunch] > nextDistance) {
+                    visited[nx][ny][isPunch] = nextDistance;
                     queue.enqueue([nx, ny, isPunch]);
-                // 1이지만 벽을 부수지 않았다면
-                } else if (matrix[nx][ny] === 1 && isPunch === 0 && visited[nx][ny][1] === 0) {
-                    visited[nx][ny][1] = visited[x][y][0] + 1;
+                }
+                // 벽이고, 벽을 부수지 않았다면
+                else if (matrix[nx][ny] === 1 && isPunch === 0 && visited[nx][ny][1] > nextDistance) {
+                    visited[nx][ny][1] = nextDistance;
                     queue.enqueue([nx, ny, 1]);
                 }
             }
         }
     }
     
-    return -1;
+    return -1; // 도달 불가능
 }
 
 console.log(bfs(0, 0));
